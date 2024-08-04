3872a2dc50fe3e0b2c4286fb3817f77441f22e6a
22dvb04 <denis22.bezzubenko04@gmail.com>
2024-08-04 11:23:35 +0100

    Sergiy correct

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ const selectQuote = document.getElementById("currency2")
 
 const buttonConvert = document.getElementById("convert")
 
-async function getPriceCurr(base, quote, amount = 1) {
+async function getPriceCurr(base, quote) {
     // https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP/AMOUNT
-    const api_key = "7643a9379eac5ef74dfef2d6";
-    const api_url = `https://v6.exchangerate-api.com/v6/${api_key}/pair/${base}/${quote}/${amount}`
-    const res = await fetch(api_url);
+    const apiKey = "7643a9379eac5ef74dfef2d6";
+    const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${base}/${quote}`
+    const res = await fetch(apiUrl);
     const data = await res.json();
-    const price = data.conversion_rate
+    const price = data.conversion_rate;
     return price
 }
 
 const checkSelected = (options) => {
-    for(const option of options){
-        if(option.selected) return option.value
+    for (const option of options) {
+        if (option.selected) return option.value
     }
 }
 
@@ -28,7 +28,7 @@ buttonConvert.addEventListener("click", async() => {
     const amount = inputBase.value;
 
     const res = await getPriceCurr(baseCurr, quoteCurr)
-    const result = amount * res
+    const result = amount * res;
     inputQuote.value = result.toFixed(2);
 })
 
